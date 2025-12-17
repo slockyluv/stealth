8afde2d355a9883a64a2e4f135b7cbe4f6826712
slockyluv <youngfalconeq@gmail.com>
2025-12-17 10:29:43 +0500

    Delete dist/src directory

--- dist/src/discord/components/selectMenus/index.js
+++
@@ -1,7 +0,0 @@
-import { settingsSectionSelect } from './settingsSection.js';
-import { settingsAutoRolesSelect } from './settingsAutoRoles.js';
-export const selectMenuHandlers = [
-    settingsSectionSelect,
-    settingsAutoRolesSelect
-];
-//# sourceMappingURL=index.js.map
--- dist/src/discord/events/index.js
+++
@@ -1,9 +0,0 @@
-import { ready } from './ready.js';
-import { interactionCreate } from './interactionCreate.js';
-import { guildMemberAdd } from './guildMemberAdd.js';
-export function registerEvents(client) {
-    client.once('ready', ready);
-    client.on('interactionCreate', interactionCreate);
-    client.on('guildMemberAdd', guildMemberAdd);
-}
-//# sourceMappingURL=index.js.map
